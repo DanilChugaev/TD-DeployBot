94e03d04b9fe48d09a5c65ab316ad6ad7d924d4c
Danil Chugaev <danilchugaev@mail.ru>
2019-02-04 00:20:54 +0700

    Removes comments

--- index.js
+++ index.js
@@ -8,24 +8,15 @@ const telegramToken = config.telegramToken;
 const travisToken = config.travisToken;
 const bot = new TelegramBot(telegramToken, {polling: true});
 
-// Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
   const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
+  const resp = match[1];
 
-  // send back the matched "whatever" to the chat
   bot.sendMessage(chatId, resp);
 });
 
-// Listen for any kind of message. There are different kinds of
-// messages.
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
-  // send a message to the chat acknowledging receipt of their message
   bot.sendMessage(chatId, 'Received your message');
 });
