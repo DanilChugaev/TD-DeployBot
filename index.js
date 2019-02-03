c367b2ec92957fc357467d36a4f8794f6b83c205
Danil Chugaev <danilchugaev@mail.ru>
2019-02-04 01:34:27 +0700

    Adds a library to use proxies for local development

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ process.env["NTBA_FIX_319"] = 1;
 
 const axios = require('axios');
 const TelegramBot = require('node-telegram-bot-api');
+const Agent = require('socks5-https-client/lib/Agent')
 const config = require(__dirname + '/config.json');
+let request = {};
+
+if (process.env.NODE_ENV !== 'production') {
+  request = {
+    agentClass: Agent,
+    agentOptions: {
+      socksHost: config.socksHost,
+      socksPort: config.socksPort,
+      socksUsername: config.socksUsername,
+      socksPassword: config.socksPassword
+    }
+  }
+}
 
 const telegramToken = config.telegramToken;
 const travisToken = config.travisToken;
-const bot = new TelegramBot(telegramToken, {polling: true});
+const bot = new TelegramBot(telegramToken, {
+  polling: true,
+  request,
+});
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
   const chatId = msg.chat.id;
--- launchBot.sh
+++ launchBot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-node index.js
+NODE_ENV=production node index.js
